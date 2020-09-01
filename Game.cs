6bd82d1c866131c19ea83aa3865d1b696c6ac122
Britt Boudreaux <brittboudreaux@yahoo.com>
2020-09-01 01:30:18 -0500

    Added backpack in progress and potion pouch

--- Game.cs
+++ Game.cs
@@ -9,25 +9,37 @@ namespace HelloWorld
         //Run the game
         public void Run()
         {
-            
+
         }
 
         //Performed once when the game begins
         public void Start()
         {
-            
+
         }
 
         //Repeated until the game ends
         public void Update()
         {
-            
+            void Battlemenu()
+            {
+                Console.WriteLine("You open your backpack which pocket do you open?");
+                Console.WriteLine("Press 1 to open potion and poison poutch");
+                Console.WriteLine("Press 2 to open books and spell tomes!");
+                Console.WriteLine("Press 3 to open small arms pocket!");
+                Console.WriteLine("Press 4 to exit the backpack!");
+                char backpackChoice = Console.ReadKey().KeyChar;
+                if (backpackChoice == '1')
+                {
+                    Console.WriteLine("You open the pouch what do you wish to use?");
+                }
+            }
         }
 
         //Performed once when the game ends
         public void End()
         {
-            
+
         }
     }
 }
